c464e9f7b50b3f065811f4132a65d222c0f1e58e
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2024-11-23 11:25:54 +0530

    Style: CSS updated

--- src/components/Meetups.jsx
+++ src/components/Meetups.jsx
@@ -36,7 +36,7 @@ const Meetups = ({searchQuery}) => {
                     <option value="Both">Both</option>
                 </select>
             </div>
-            <div className="row d-flex justify-content-start">
+            <div className="row">
             {displayedData.map((card) => (
                 <div key={card._id} className="col-md-4 container mb-5">
                     <div className="mt-3">
